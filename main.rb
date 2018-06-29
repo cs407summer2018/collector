e6afb07f0ccaae00fb14b9d100a85e551d619847
Rex Suter <dale@pal-nat186-82-227.itap.purdue.edu>
2018-06-29 11:53:51 -0400

    update

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'net/ssh'
 require 'optparse'
 require 'mysql2'
 require 'yaml'
+require 'time'
+require 'date'
 
 keys = YAML::load_file("env.conf")
 
@@ -19,7 +21,7 @@ username = keys["cli"]["username"]
 password = keys["cli"]["password"]
 cmd = "who"
 
-def get_existing_or_create_user(user)
+def get_existing_or_create_user(client, user)
   q_string = "SELECT * FROM user WHERE name = '#{user}';"
   results = client.query(q_string)
 
@@ -33,34 +35,68 @@ def get_existing_or_create_user(user)
   q_string = "SELECT * FROM user WHERE name = '#{user}';"
   results = client.query(q_string)
 
-  results[0]
+  results.first
 end
 
-def get_existing_machine(name)
+def get_existing_machine(client, name)
   q_string = "SELECT * FROM machine WHERE name = '#{name}';"
   results = client.query(q_string)
-  results[0]
+  results.first
+end
+
+def get_current_machine_sessions(client, machine_name)
+  machine_rec = get_existing_machine(client, machine_name)
+  machine_id = machine_rec["id"]
+  puts "machine_id"
+  puts machine_id
+  q_string = "SELECT * FROM session WHERE machine_id = #{machine_id} AND end_time IS NULL;"
+  results = client.query(q_string)
+  results
 end
 
 Net::SSH.start(hostname, username, password: password) do |ssh|
   res = ssh.exec!(cmd)
   ssh.close
 
+  machine_record = get_existing_machine(client, machine_name)
+  puts machine_record
+
+  # query for current sessions for machine
+  prev_sessions = get_current_machine_sessions(client, machine_name)
+  prev_sessions_match_cur_users = []
+  prev_sessions_not_match_users  = []
+
   res.split("\n").each do |line|
-    puts "#########"
     elems = line.split(" ") # skip 5th item?
     user = elems[0]
     tty  = elems[1]
     date = elems[2]
     time = elems[3]
+    dt_str = "#{date} #{time}"
+    datetime = DateTime.parse(dt_str)
 
-    puts "user: "+user
-    puts "tty: "+tty
-    # puts "date: "+date
-    # puts "time: "+time
+    user_record = get_existing_or_create_user(client, user)
+    puts user_record
 
-    user_record = get_existing_or_create_user(user)
-    machine_record = get_existing_machine(machine_name)
+    # query for existing session for user/machine/start_time combo
+    prev_sessions.each do |sess_record|
+      suid = sess_record.user_id
+      user_id = user_record.id
+      if suid = user_id
+        pre_sessions_match_cur_users << thing
+      else
+        pre_sessions_not_match_users << thing
+      end
+    end
+    #  { machine_id: 3, user: james }
+    #  { machine_id: 3, user: flynn }
+
+    # user_id matches
+    # if same start_time update the record (leave it?)
+    # if diff start_time:
+    #    mark old as ended
+    #    create record for new
+
 
     if tty.include?("tty")
       # normal login
@@ -69,5 +105,6 @@ Net::SSH.start(hostname, username, password: password) do |ssh|
       # ssh session
     end
   end
-
+  # make it through current users and never matched
+  # so not logged in: mark end time as now & update record
 end
