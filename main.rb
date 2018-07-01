69e73bb83ba7b5fa6d486356ba5312bd378b6420
Rex Suter <dale@Rexs-MacBook-Air.local>
2018-07-01 01:48:02 -0400

    rm old main

--- main.rb
+++ main.rb
@@ -1,115 +1,66 @@
-require 'rubygems'
-require 'net/ssh'
-require 'optparse'
+require "rubygems"
 require 'mysql2'
-require 'yaml'
-require 'time'
-require 'date'
+require "active_record"
+require 'net/ssh'
+
+load "models.rb"
 
 keys = YAML::load_file("env.conf")
 
-client = Mysql2::Client.new(
-  :host => keys["db"]["host"],
-  :username => keys["db"]["username"],
-  :password => keys["db"]["password"],
-  :database => keys["db"]["database"]
-)
+@machine_suffix = ".cs.purdue.edu"
+@email_suffix   = "@purdue.edu"
+@username = keys["cli"]["username"]
+@password = keys["cli"]["password"]
+@cmd      = "who"
 
-machine_name = keys["cli"]["machine_name"]
-hostname = "#{machine_name}.cs.purdue.edu"
-username = keys["cli"]["username"]
-password = keys["cli"]["password"]
-cmd = "who"
+ActiveRecord::Base.
+  establish_connection(
+    {:adapter => "mysql2",
+     :host => keys["db"]["host"],
+     :username => keys["db"]["username"],
+     :password => keys["db"]["password"],
+     :database => keys["db"]["database"]})
 
-def get_existing_or_create_user(client, user)
-  q_string = "SELECT * FROM user WHERE name = '#{user}';"
-  results = client.query(q_string)
-
-  if results.size == 0
-    # insert the user in the database
-    i_string = "INSERT INTO user ( name, email ) VALUES ( \"#{user}\", \"#{user}@purdue.edu\" ) ;"
-    puts i_string
-    client.query(i_string)
-  end
-
-  q_string = "SELECT * FROM user WHERE name = '#{user}';"
-  results = client.query(q_string)
-
-  results.first
+def parse_line_of_cmd_result(line, machine_name)
+  data = line.split(" ")
+  user = data[0]
+  usage = {
+    host: machine_name,
+    user: user,
+    type: data[1],
+    datetime: DateTime.parse("#{data[2]} #{data[3]}")
+  }
+  User.where(name: user, email: "#{user}#{@email_suffix}").first_or_create
+  usage
 end
 
-def get_existing_machine(client, name)
-  q_string = "SELECT * FROM machine WHERE name = '#{name}';"
-  results = client.query(q_string)
-  results.first
-end
-
-# def get_current_machine_sessions(client, machine_name)
-#   machine_rec = get_existing_machine(client, machine_name)
-#   machine_id = machine_rec["id"]
-#   puts "machine_id"
-#   puts machine_id
-#   q_string = "SELECT * FROM session WHERE machine_id = #{machine_id} AND end_time IS NULL;"
-#   results = client.query(q_string)
-#   results
-# end
-
-Net::SSH.start(hostname, username, password: password) do |ssh|
-  res = ssh.exec!(cmd)
-  ssh.close
-
-  machine_record = get_existing_machine(client, machine_name)
-  puts machine_record
-
-  # query for current sessions for machine
-  # prev_sessions = get_current_machine_sessions(client, machine_name)
-  # prev_sessions_match_cur_users = []
-  # prev_sessions_not_match_users  = []
-  who_data = []
-
+def handle_ssh_output(res, machine_name)
+  return if res.blank? # machine has no users
+  usages = []
   res.split("\n").each do |line|
-    elems = line.split(" ") # skip 5th item?
-    date = elems[2]
-    time = elems[3]
-    datetime = DateTime.parse("#{date} #{time}")
-    item = {
-      user: elems[0],
-      tty: elems[1],
-      datetime: datetime
-    }
-    who_data << item
+    usage = parse_line_of_cmd_result line, machine_name
+    usages << usage
+    puts usage
   end
+  # TODO: Update or insert usage record as necessary
+end
 
-  user_record = get_existing_or_create_user(client, user)
-  puts user_record
-
-  # query for existing session for user/machine/start_time combo
-  prev_sessions.each do |sess_record|
-    suid = sess_record.user_id
-    user_id = user_record.id
-    if suid = user_id
-      pre_sessions_match_cur_users << thing
-    else
-      pre_sessions_not_match_users << thing
-    end
+def ssh_wrapper(machine_name)
+  host_name = "#{machine_name}#{@machine_suffix}"
+  puts "Attempting to SSH into #{host_name}"
+  Net::SSH.start(host_name, @username, password: @password) do |ssh|
+    res = ssh.exec!(@cmd)
+    handle_ssh_output res, machine_name
+    ssh.close
   end
+end
 
-  if tty.include?("tty")
-    # normal login
-  end
-  if tty.include?("pts")
-    # ssh session
-    # end
-  end
-  #  { machine_id: 3, user: james }
-  #  { machine_id: 3, user: flynn }
+def run_for_machine(machine)
+  os = machine.room.specifications.first.OS
+  return if os != "Linux"
+  ssh_wrapper machine.name
+end
 
-  # user_id matches
-  # if same start_time update the record (leave it?)
-  # if diff start_time:
-  #    mark old as ended
-  #    create record for new
-
-  # make it through current users and never matched
-  # so not logged in: mark end time as now & update record
+Machine.all.each do |machine|
+  run_for_machine machine
 end
